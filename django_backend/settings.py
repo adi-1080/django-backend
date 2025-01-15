a5a1e22238e215cd6c9fb210e136536c1f045d3e
adi-1080 <adityagupta5277@gmail.com>
2025-01-15 18:58:15 +0530

    dj

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://django-backend-yuzj.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['https://django-backend-sjfj.onrender.com', 'localhost']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
